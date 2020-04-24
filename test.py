522fc63c1f2816f941f91dfc00a41031a7651945
sahilgupta_gti <sahilgupta_gti@yahoo.com>
2020-04-25 01:07:50 +0530

    commit local file1 2ndtime

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
 #! /bin/python
-print "hello world"
+from numpy import *
+# from array import *
+import sys
+
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+
+
+def factorial(f):
+    if f == 1:
+        return f
+    return f * factorial(f-1)
+
+
+fact = 6
+print(factorial(fact))
+
+print(__name__)
